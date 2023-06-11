a833ebde507a9f30969d03ad81bbc5f58636e0c1
oreganoli <3611916+oreganoli@users.noreply.github.com>
2023-06-11 15:57:46 +0200

    feat: train linear regression model based on data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 
 if __name__ == "__main__":
     data = pd.read_csv("data/housing.csv", sep="\ +", engine="python")
@@ -18,4 +19,23 @@ if __name__ == "__main__":
         "LowerClassPopulationProportion",
         "MedianValueInThousands"
     ]
-    print(data)
+    # Split dataset into inputs and output
+    X = data[[
+        "PerCapitaCrimeRate",
+        "LargeResidentialZoningProportion",
+        "IndustrialProportion",
+        "BordersRiver",
+        "NitricOxideConcentration",
+        "RoomsPerDwelling",
+        "Pre1940ConstructionProportion",
+        "CommuteDistance",
+        "RadialHighwayAccessibility",
+        "TaxRate",
+        "PupilsToTeachers",
+        "BlackPopulationScore",
+        "LowerClassPopulationProportion",
+    ]]
+    y = data["MedianValueInThousands"]
+    # Train model
+    model = LinearRegression()
+    model.fit(X, y)
