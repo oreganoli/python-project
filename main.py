e27f369752bc1185daab910737f2beaf1062e0c1
oreganoli <3611916+oreganoli@users.noreply.github.com>
2023-06-11 15:28:15 +0200

    feat: load Boston housing dataset

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+import pandas as pd
+
 if __name__ == "__main__":
-    print("hello world")
+    data = pd.read_csv("data/housing.csv", sep="\ +", engine="python")
+    data.columns = [
+        "PerCapitaCrimeRate",
+        "LargeResidentialZoningProportion",
+        "IndustrialProportion",
+        "BordersRiver",
+        "NitricOxideConcentration",
+        "RoomsPerDwelling",
+        "Pre1940ConstructionProportion",
+        "CommuteDistance",
+        "RadialHighwayAccessibility",
+        "TaxRate",
+        "PupilsToTeachers",
+        "BlackPopulationScore",
+        "LowerClassPopulationProportion",
+        "MedianValueInThousands"
+    ]
+    print(data)
